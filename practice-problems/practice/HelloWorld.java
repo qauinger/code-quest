76cb30a35f58e35ccf85eb6ef3ccd532f31e85d6
qauinger <92843977+qauinger@users.noreply.github.com>
2022-04-08 11:28:39 -0400

    Add Hello World
    https://lmcodequestacademy.com/api/static/problems/hello-world

---
+++ practice-problems/practice/HelloWorld.java
@@ -0,0 +1,13 @@
+// https://lmcodequestacademy.com/api/static/problems/hello-world
+import java.util.Scanner;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    try (Scanner scanner = new Scanner(System.in)) {
+      int inputLines = Integer.parseInt(scanner.nextLine());
+      for(int line = 0; line < inputLines; line++) {
+        System.out.println(scanner.nextLine());
+      }
+    }
+  }
+}
