65d2c4c6e9485a47fbbad6468c20d067ec81a5af
qauinger <92843977+qauinger@users.noreply.github.com>
2022-04-08 13:21:24 -0400

    Add Not So Self-Driving
    https://lmcodequestacademy.com/api/static/problems/not-so-self-driving

---
+++ practice-problems/practice/not-so-self-driving/NotSoSelfDriving.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class NotSoSelfDriving {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int inputLines = Integer.parseInt(scanner.nextLine());
+        for(int i = 0; i < inputLines; i++) {
+            String line = scanner.next();
+            String[] split = line.split(":", 2);
+            double speed = Double.parseDouble(split[0]);
+            double distance = Double.parseDouble(split[1]);
+            if(speed >= distance) {
+                System.out.println("SWERVE");
+            } else if((5 * speed) >= distance) {
+                System.out.println("BRAKE");
+            } else {
+                System.out.println("SAFE");
+            }
+        }
+    }
+}
